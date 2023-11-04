80f3ce2f990a84ab8ed155334ecb48e9671cc973
Pharadorn13 <149857450+Pharadorn13@users.noreply.github.com>
2023-11-04 15:21:05 +0700

    Update SCADA_app.py

--- SCADA_app.py
+++ SCADA_app.py
@@ -1,5 +1,5 @@
 import streamlit as s
-s.title("SUNSTAR")^
+s.title("SUNSTAR")
 s.subheader("SUNSTAR ENGINEERING THAILAND")
 s.header("GRINDING")
 s.text("DISPLAY REALTIME STATUS FROM NISSEI MACHINE")
